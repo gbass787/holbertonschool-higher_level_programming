b59aeccb0c2e2cbb2114553ad77c341be339b39e
gbass787 <4551@holbertonschool.com>
2022-09-10 05:06:27 +0000

    fixxx

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv
-
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-
-    if len(argv) != 4:
+    length = len(argv)
+    arg = argv
+    if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(argv[1])
-    b = int(argv[3])
+    a = int(arg[1])
+    b = int(arg[3])
+    sign = arg[2]
 
-    if (argv[2] == '+'):
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
-    elif (argv[2] == '-'):
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-
-    elif (argv[2] == '*'):
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-
-    elif (argv[2] == '/'):
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-
+    if sign == "+":
+        d = add(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, sign, b, d))
+    elif sign == "-":
+        d = sub(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, sign, b, d))
+    elif sign == "*":
+        d = mul(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, sign, b, d))
+    elif sign == "/":
+        d = div(a, b)
+        print("{:d} {:s} {:d} = {:d}".format(a, sign, b, d))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("Unkown operator. Available operators: +, -, * and /")
         exit(1)
