cc6e78d761f6b65e4f12e3cb8eee116f9667d03f
gbass787 <4551@holbertonschool.com>
2022-09-16 09:27:27 +0000

    tasks 5-8

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum = [0, 0]
+    for i in range(2):
+        try:
+            sum[i] += tuple_a[i]
+        except:
+            sum[i] += 0
+            try:
+                sum[i] += tuple_b[i]
+            except:
+                sum[i] += 0
+                return tuple(sum)
