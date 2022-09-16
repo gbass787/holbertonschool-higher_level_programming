29ed417d73fa6354b0ab91d66af54cee5571696e
gbass787 <4551@holbertonschool.com>
2022-09-16 09:32:33 +0000

    fix task 7 & 8

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,8 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
             sum[i] += tuple_a[i]
         except:
             sum[i] += 0
-            try:
-                sum[i] += tuple_b[i]
-            except:
-                sum[i] += 0
-                return tuple(sum)
+        try:
+            sum[i] += tuple_b[i]
+        except:
+            sum[i] += 0
+    return tuple(sum)
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length != 0;
-    return length, sentence[0]
-return length, None
+    if length != 0:
+        return length, sentence[0]
+    return length, None
