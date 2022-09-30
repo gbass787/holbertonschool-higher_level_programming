38e51367d4ad1c72d9e02470fbf40c67dea094d1
gbass787 <4551@holbertonschool.com>
2022-09-30 20:28:36 +0000

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-'''adds all argument'''
+'''comments'''
 
 
-import json
 import sys
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+import json
 
-filename = "add_item.json"
-lists = []
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    lists = load(filename)
-except:
-    pass
-for i in range(1, len(sys.argv)):
-    lists.append(sys.argv[i])
-save(lists, filename)
+list_a = []
+
+for arg in range(1, len(sys.argv)):
+    list_a.append(sys.argv[arg])
+
+save_to_json_file(list_a, "add_item.json")
