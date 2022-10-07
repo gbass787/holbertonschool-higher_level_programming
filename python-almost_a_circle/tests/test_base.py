ef713f4d81e9868c54e6d576a2592ae0bdefb2f3
gbass787 <4551@holbertonschool.com>
2022-10-07 22:35:25 +0000

    fixing tests

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Unittest for Base'''
+"""unittest base"""
 
 import unittest
 import os
--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Unittest for Square'''
+"""unittest square"""
 
 import unittest
 import io
