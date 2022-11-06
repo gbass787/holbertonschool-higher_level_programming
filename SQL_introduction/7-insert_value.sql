2a3acb72716efa189bf0bfbe4c76c1836e5de9d4
gbass787 <4551@holbertonschool.com>
2022-11-06 12:35:27 +0000

    fixed dumb error

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Script that inserts a new row in the table 'first_table' found in database 'hbtn_0c_0' in my MySQL server
 -- New row description: id = 89, name = Best School
 INSERT INTO `first_table` (`id`, `name`)
-VALUES (89, "Holberton School");
+VALUES (89, "Best School");
