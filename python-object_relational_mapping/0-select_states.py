b28f1b334c83375985203bec1bc8c99921ba4d77
gbass787 <4551@holbertonschool.com>
2022-11-11 07:37:26 +0000

    fix task 0-1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,19 +4,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect('localhost', user, passwd, database, 3306)
-
-    try:
-        c = db.cursor()
-        c.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = c.fetchall()
-        for row in rows:
-            print(row)
-    except:
-        db.rollback()
-
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,14 +4,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    database = sys.argv[3]
-    db = MySQLdb.connect('localhost', user, passwd, database, 3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY id ASC""")
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    db.close()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
